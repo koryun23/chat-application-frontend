190a5ee4e3f47e7b50b06ef41b940c0025fd035e
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2023-03-15 20:21:08 +0400

    implementing the manu option box

---
+++ src/components/home/MenuOptions.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+import "../../css/home/MenuOptions.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function MenuOptions(props) {
+
+    const [options, setOptions] = useState(["Profile", "Sign Out"]);
+    const [icons, setIcons] = useState([faUser, faPowerOff]);
+
+    const clickMenuOption = (index) => {
+        console.log(options[index]);
+    }
+    const menuItems = icons.map((icon, index) => (
+        <div className="menu-option" id={index} onClick={() => clickMenuOption(index)}>
+            <div className="option-icon">
+                {<FontAwesomeIcon icon={icon} size="lg" />}
+            </div>
+            <div className="option-name-area">
+                <b>{options[index]}</b>
+            </div>
+        </div>
+    ));
+
+    return (
+        <div className={props.showMenuOptions ? "show-menu-options" : "show-menu-options no-display"}>
+            {menuItems}
+        </div>
+    );
+}
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -3,9 +3,12 @@ import "../../css/home/HomePage.css";
 import { faAdd, faEdit, faLineChart, faNavicon, faPlus, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
+import MenuOptions from "../../components/home/MenuOptions";
+
 export default function HomePage(props) {
 
     const [sidebarOnHover, setSidebarOnHover] = useState(false);
+    const [showMenuOptions, setShowMenuOptions] = useState(false);
 
     const onLogOut = () => {
         localStorage.clear();
@@ -15,12 +18,16 @@ export default function HomePage(props) {
     // 1) profile
     // 2) sign out
 
+    const onClickMenu = () => {
+        setShowMenuOptions(!showMenuOptions);
+        console.log("clicked");
+    }
     return (
         <div className="main">
             <div className="sidebar" onMouseEnter={() => setSidebarOnHover(true)} onMouseLeave={() => setSidebarOnHover(false)}>
                 <div className="commands">
                     <div className="menu">
-                        <button className="menu-button">
+                        <button className="menu-button" onMouseDown={onClickMenu}>
                             <FontAwesomeIcon icon={faNavicon} size="lg"/>
                         </button>
                     </div>
@@ -31,16 +38,16 @@ export default function HomePage(props) {
                 </div>
 
                 <div className="chats">
+                    <MenuOptions showMenuOptions={showMenuOptions} />
 
-                </div>
-                <div className="selected-chat">
                     <button className={sidebarOnHover ? "add-chat-button" : "add-chat-button no-display"}>
                         <FontAwesomeIcon icon={faEdit} size="lg" />
                     </button>
-                </div>       
+                </div>     
             </div>
+            <div className="selected-chat">
 
-
+            </div>  
         </div>
     );
 }
