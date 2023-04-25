172ec37668dfcbe5971ea56c624121aa15fd8458
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2023-04-25 20:32:39 +0400

    prettifying the display of the messages

--- src/components/home/Message.js
+++ src/components/home/Message.js
@@ -8,14 +8,16 @@ export default function Message(props) {
     return (
         <div className={props.sentBySelf ? "container darker" : "container"}>
             <div className={props.sentBySelf ? "message-box sent-by-self" : "message-box"}>
-                {!props.sentBySelf && <p className="sender">{props.message.username}</p>}
-                <p>{props.message.message}</p>
-                <span className={props.sentBySelf ? "time-right" : "time-right"}>{props.message.sentAt.slice(11, 16)}</span>
-                {/* {props.sentBySelf && 
-                <div className="avatar">
-                    <FontAwesomeIcon icon={faUser} size="2x"/>
+                {
+                    !props.sentBySelf && 
+                    <div className="sender-container">
+                        <p className="sender">{props.message.username}</p>
+                    </div>
+                }
+                <div className={!props.sentBySelf ? "message-text-container" : ""}>
+                    <p>{props.message.message}</p>
                 </div>
-                } */}
+                <span className={props.sentBySelf ? "time-right" : "time-right"}>{props.message.sentAt.slice(11, 16)}</span>
             </div>
 
         </div>
